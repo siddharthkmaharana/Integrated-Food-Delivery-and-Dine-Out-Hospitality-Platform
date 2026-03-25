70b7f243d1999cb5dd1173468c9fe0c182f92041
Pradhanrahul15 <pradhanrahul1515@gmail.com>
2026-03-25 17:20:02 +0530

    feat: update the server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,30 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import restaurantRoutes from './routes/restaurantRoutes.js';
+import socketHandler from './sockets/socketHandler.js';
 
 connectDB();
 
 const app = express();
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+});
+
+// Make io accessible in controllers
+app.set('io', io);
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,8 +34,12 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 
+// Socket handler
+socketHandler(io);
+
 // Health check
 app.get('/', (req, res) => res.json({ message: 'DineOut API is running' }));
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
